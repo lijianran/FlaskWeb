a4f3d3c92c3ea30c758d2fead472e30b4bd0d3f7
王昊 <275026569@qq.com>
2020-07-13 11:27:33 +0800

    第五次提交完成基本信息表格

--- wanghao/flask-tutorial/flaskr/db.py
+++ wanghao/flask-tutorial/flaskr/db.py
@@ -4,6 +4,7 @@ import click
 from flask import current_app, g
 from flask.cli import with_appcontext
 
+import os
 
 def get_db():
     if 'db' not in g:
@@ -14,7 +15,6 @@ def get_db():
         g.db.row_factory = sqlite3.Row
 
     return g.db
-
 
 def close_db(e=None):
     db = g.pop('db', None)
@@ -28,14 +28,49 @@ def init_db():
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
-
 @click.command('init-db')
 @with_appcontext
 def init_db_command():
-    """Clear the existing data and create new tables."""
     init_db()
     click.echo('Initialized the database.')
 
+########################################################
+
+def get_lijing_db():
+    if 'lijing_db' not in g:
+        g.lijing_db = sqlite3.connect(
+            os.path.join(current_app.instance_path, 'lijing.sqlite'),
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.lijing_db.row_factory = sqlite3.Row
+
+    return g.lijing_db
+
+def close_lijing_db(e=None):
+    db = g.pop('lijing_db', None)
+
+    if db is not None:
+        db.close()
+
+def init_lijing_db():
+    db = get_lijing_db()
+
+    with current_app.open_resource('lijing.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+@click.command('init-lijing-db')
+@with_appcontext
+def init_lijing_db_command():
+    init_lijing_db()
+    click.echo('Initialized the lijing database.')
+
+
+
+
+########################################################
+
 def init_app(app):
     app.teardown_appcontext(close_db)
+    app.teardown_appcontext(close_lijing_db)
     app.cli.add_command(init_db_command)
+    app.cli.add_command(init_lijing_db_command)
--- wanghao/flask-tutorial/flaskr/lijing/lijing_basicinfo.py
+++ wanghao/flask-tutorial/flaskr/lijing/lijing_basicinfo.py
@@ -6,7 +6,7 @@ from werkzeug.utils import secure_filename
 
 from flaskr.auth import login_required
 
-#from flaskr.db import get_db, get_lijing_db
+from flaskr.db import get_db, get_lijing_db
 
 #from flaskr.lijing.lijing_index import float_int_string
 
@@ -15,7 +15,7 @@ from flaskr.auth import login_required
 # import xlrd
 # import xlsxwriter
 # import json
-# import datetime
+import datetime
 # from pypinyin import lazy_pinyin
 
 bp = Blueprint('lijing_basicinfo', __name__, url_prefix='/lijing_basicinfo')
@@ -24,4 +24,57 @@ bp = Blueprint('lijing_basicinfo', __name__, url_prefix='/lijing_basicinfo')
 @bp.route('/hello')
 @login_required
 def hello():
-    return render_template('lijing/basicInfo.html')
+    db = get_lijing_db()
+
+    year_data = db.execute('select year from year_list').fetchall()
+    year_new = datetime.datetime.now().year
+    year_list = []
+    if len(year_data) == 0:
+        year_list = ['暂无数据']
+    else:
+        for year in year_data:
+            year_list.insert(0, year['year'])
+        year_new = int(year_list[0]) + 1
+        # if 'year_current' not in session:
+        session['year_current'] = year_list[0]
+
+    return render_template('lijing/basicInfo.html', year_list=year_list, year_new=year_new)
+
+
+
+@bp.route('/jsondata', methods=('GET', 'POST'))
+def jsondata():
+
+    db = get_lijing_db()
+
+    data = []
+
+    try:
+        result = db.execute('select person_id, person_name, gender from person_' +
+                            session['year_current']).fetchall()
+        for row in result:
+            d = {}
+            d['id'] = row['person_id']
+            d['name'] = row['person_name']
+            d['gender'] = row['gender']
+            data.append(d)
+    except:
+        return jsonify({'total': len(data), 'rows': data})
+
+    if request.method == 'POST':
+        print('post')
+    if request.method == 'GET':
+        info = request.values
+        limit = info.get('limit', 10)  # 每页显示的条数
+        offset = info.get('offset', 0)  # 分片数，(页码-1)*limit，它表示一段数据的起点
+        # print('get', limit)
+        # print('get  offset', offset)
+        return jsonify({'total': len(data), 'rows': data[int(offset):(int(offset) + int(limit))]})
+        # 注意total与rows是必须的两个参数，名字不能写错，total是数据的总长度，rows是每页要显示的数据,它是一个列表
+        # 前端根本不需要指定total和rows这俩参数，他们已经封装在了bootstrap table里了
+
+
+
+@bp.route('/importData', methods=('GET', 'POST'))
+def importData():
+  return redirect(url_for('lijing_basicinfo.hello'))
