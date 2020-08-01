188e9ecc25dc009f318b7f657f7b76f706fe8fe1
简然 李 <lijianran@outlook.com>
2020-08-01 21:45:37 +0800

    李简然第四次提交

--- lijianran/flask-tutorial/hello.py
+++ lijianran/flask-tutorial/hello.py
@@ -1,10 +1,8 @@
-# from flask import Flask
-from flask import Flask
 
+from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
+def hello_world():
     return 'Hello, World!'
