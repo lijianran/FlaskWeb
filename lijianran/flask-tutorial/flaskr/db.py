dfb3ea769bc3056c19ffa4e1d6580b20f037df97
简然 李 <lijianran@outlook.com>
2020-08-07 17:20:04 +0800

    李简然的第六次提交，完成了欢迎界面

--- lijianran/flask-tutorial/flaskr/db.py
+++ lijianran/flask-tutorial/flaskr/db.py
@@ -4,6 +4,7 @@ import click
 from flask import current_app, g
 from flask.cli import with_appcontext
 
+import os
 
 def get_db():
     if 'db' not in g:
@@ -39,7 +40,45 @@ def init_db_command():
     click.echo('初始化了数据库')
 
 
+########################################################################
+
+
+def get_lijing_db():
+    if 'lijing_db' not in g:
+        g.lijing_db = sqlite3.connect(
+            os.path.join(current_app.instance_path, 'lijing.sqlite'),
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.lijing_db.row_factory = sqlite3.Row
+
+    return g.lijing_db
+
+
+def close_lijing_db(e=None):
+    db = g.pop('lijing_db', None)
+
+    if db is not None:
+        db.close()
+
+
+def init_lijing_db():
+    db = get_lijing_db()
+
+    with current_app.open_resource('lijing.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-lijing-db')
+@with_appcontext
+def init_lijing_db_command():
+    init_lijing_db()
+    click.echo('Initialized the lijing database.')
+
+
 def init_app(app):
     app.teardown_appcontext(close_db)
+    app.teardown_appcontext(close_lijing_db)
     app.cli.add_command(init_db_command)
+    app.cli.add_command(init_lijing_db_command)
+
 
